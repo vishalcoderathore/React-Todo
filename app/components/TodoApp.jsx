801668445a84159233fecefa60ec81b27f073c1c
Vishal <vshl.rathore1@gmail.com>
2016-08-17 18:50:55 -0400

    Added TodoApp (Main) Component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render : function(){
+    return(
+      <div>
+        <h1>TodoApp.</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
