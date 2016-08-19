0810126ac920f6b5c52014f0f3a46a4ede3553b6
Vishal <vshl.rathore1@gmail.com>
2016-08-19 00:00:06 -0400

    Added test cases for search component

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TosoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist();
+  });
+
+  it('should call onSearch with entered input text', () =>  {
+    var testData = "Test Data";
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    todoSearch.refs.searchText.value = testData;
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, testData);
+
+  });
+
+  it('should call onSearch with proper checked value', () =>  {
+    var testData = "";
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, testData);
+
+   });
+
+});
